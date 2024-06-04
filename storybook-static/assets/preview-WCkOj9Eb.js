f8bf34babb37653f7e88c2f9b361e15a6a191a74
0b1kn00b <polysemantic@gmail.com>
2024-06-04 16:34:27 +0200

    publish

---
+++ storybook-static/assets/preview-WCkOj9Eb.js
@@ -0,0 +1,1 @@
+const e={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},docs:{toc:!0},layout:"fullscreen",darkMode:{stylePreview:!0},tags:["autodocs"]}};export{e as default};
